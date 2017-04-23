5f09d05e9d5f3b713e3337213d331513f654222c
h4ckathon <vorltecs@gmail.com>
2017-04-23 03:22:24 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,12 +7,12 @@ function solve(){
 function getSyncResponse(coder, challenge, test){
   var data = {coder:coder, test:test};
   if(challenge != null && challenge != ''){
-    data = $.extend({}, data, {challenge: challenge});
+    $.extend(data, {challenge: challenge});
   }
   $.ajax({
     url: 'https://ac-challenge.herokuapp.com/api/challenge',
     async: true,
-    data: ,
+    data: data,
     success: function(a,b,c){console.log(a);console.log(b);console.log(c);},
     error: function(a,b,c){console.log(a);console.log(b);console.log(c);}
   }); 
