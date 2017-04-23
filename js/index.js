c8fd3fcd360142ed5b6b1d0331978dfc3d79401f
h4ckathon <vorltecs@gmail.com>
2017-04-23 03:09:19 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 function solve(){
   var coder = $('#email').val();
+  getSyncResponse(coder, null, true);
   confirm('Test for ' + coder + ' successful!');
+}
+
+function getSyncResponse(coder, challenge, test){
+  $.ajax({
+    url: 'https://ac-challenge.herokuapp.com/api/challenge',
+    async: true,
+    data: {coder:coder, challenge:challenge, test:test},
+    success: function(a,b,c){console.log(a);console.log(b);console.log(c);}
+  }); 
 }
