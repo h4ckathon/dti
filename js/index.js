eff65611eff55ebeea122372888a7d218eab5d7f
h4ckathon <vorltecs@gmail.com>
2023-10-26 17:58:51 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,33 @@
 function solve(){
   var coder = $('#email').val();
-  const settings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://onecompiler-apis.p.rapidapi.com/api/v1/run',
-	method: 'POST',
-	headers: {
-		'content-type': 'application/json',
-		'X-RapidAPI-Key': '216af59996mshb83318e70b90962p18bd1fjsnfe56b4d521df',
-		'X-RapidAPI-Host': 'onecompiler-apis.p.rapidapi.com'
-	},
-	processData: false,
-	data: '{\r\n    "language": "python",\r\n    "stdin": "Peter",\r\n    "files": [\r\n        {\r\n            "name": "index.py",\r\n            "content": "import sys\nname = sys.stdin.readline()\nprint(\'Hello \'+ name)"\r\n        }\r\n    ]\r\n}'
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
+const data = JSON.stringify({
+	language: 'python',
+	stdin: 'Peter',
+	files: [
+		{
+			name: 'index.py',
+			content: 'import sys
+name = sys.stdin.readline()
+print(\'Hello \'+ name)'
+		}
+	]
 });
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open('POST', 'https://onecompiler-apis.p.rapidapi.com/api/v1/run');
+xhr.setRequestHeader('content-type', 'application/json');
+xhr.setRequestHeader('X-RapidAPI-Key', '216af59996mshb83318e70b90962p18bd1fjsnfe56b4d521df');
+xhr.setRequestHeader('X-RapidAPI-Host', 'onecompiler-apis.p.rapidapi.com');
+
+xhr.send(data);
 alert("done, ${coder}")
   
 }
