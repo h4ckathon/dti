2408f07eab788f7636c75ab91209d1bc23aef16a
h4ckathon <vorltecs@gmail.com>
2023-10-26 17:53:20 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,79 +1,22 @@
-var initialAlfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-                      "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-                      "0","1","2","3","4","5","6","7","8","9","_","="];
-var alfabet;
-var challenge;
-var stringSize;
-var response;
-var responseCode;
 function solve(){
   var coder = $('#email').val();
-  getSyncResponse(coder, '', true);
-  setFirstChallenge();
-  var challengeAccepted = true;
-  var counter = 0;
-  do{
-    console.log("#"+(counter+1));
-    getSyncResponse(coder, challenge, true);
-    challengeAccepted = verifyAndGenerateNewChallenge();
-    counter++;
-  } while(!challengeAccepted && counter < initialAlfabet.length+10);
-  if(!challengeAccepted){
-    alert("An error occurred during the execution.\nPlease try again, and if the error persists contact-me at vorltecs@gmail.com")
-  } else{
-    getSyncResponse(coder, challenge, false);
-    alert(response.responseText);
-  }
-}
+  const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://onecompiler-apis.p.rapidapi.com/api/v1/run',
+	method: 'POST',
+	headers: {
+		'content-type': 'application/json',
+		'X-RapidAPI-Key': '216af59996mshb83318e70b90962p18bd1fjsnfe56b4d521df',
+		'X-RapidAPI-Host': 'onecompiler-apis.p.rapidapi.com'
+	},
+	processData: false,
+	data: '{\r\n    "language": "python",\r\n    "stdin": "Peter",\r\n    "files": [\r\n        {\r\n            "name": "index.py",\r\n            "content": "import sys\nname = sys.stdin.readline()\nprint(\'Hello \'+ name)"\r\n        }\r\n    ]\r\n}'
+};
 
-function getSyncResponse(coder, challenge, test){
-  var data = {coder:coder, challenge:challenge.toString().replace(/,/g,""), test:test};
-  $.ajax({
-    url: 'https://ac-challenge.herokuapp.com/api/challenge',
-    async: false,
-    data: data,
-    success: function(code, msg, obj){console.log("responseMsg: " + msg);responseCode = code.split(""); response = obj;},
-    error: function(error){if(error.status == 409){stringSize = parseInt(error.responseText);}}
-  }); 
-}
-
-function setFirstChallenge(){
-  alfabet = initialAlfabet.slice(0);
-  challenge = [];
-  for(var i=0; i<stringSize; i++){
-    var pos = i;
-    if(i>=alfabet.length){
-      pos = i%alfabet.length;
-    }
-    challenge[i] = alfabet[pos];
-  }
-  console.log("Initial alfabet: " + alfabet);
-  console.log("Challenge size discovered: " + stringSize);
-  console.log("First challenge: " + challenge);
-}
-
-function verifyAndGenerateNewChallenge(){
-  console.log("responseStatus: " + response.status);
-  console.log("responseCode:   " + responseCode);
-  console.log("challenge used: " + challenge);
-  if(response.status == 206){
-    for(var i = stringSize-1; i>=0; i--){
-      if(responseCode[i] != "R"){
-        var alfabetIndex = alfabet.indexOf(challenge[i]);
-        var nextAlfabetIndex = alfabetIndex+1;
-        if(nextAlfabetIndex >= alfabet.length){
-          nextAlfabetIndex = 0;
-        }
-        challenge[i] = alfabet[nextAlfabetIndex];
-        if(responseCode[i] == "W" && alfabetIndex > -1){
-          alfabet.splice(alfabetIndex,1);
-        }
-      }
-    }
-    console.log("next challenge: " + challenge);
-    return false;
-  } else{
-    console.log(response);
-    return true;
-  }
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+alert("done!")
+  
 }
