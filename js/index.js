a9e5eb8244720fcbde71a2ff1d5c48bf6cb0ea42
h4ckathon <vorltecs@gmail.com>
2023-10-31 11:36:49 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,39 @@
 {
-	var numberOfQuestions = 0;
-	var numberOfSuccess = 0;
-	var a = [];
+	var article = `<article style='position: relative; width: 100%; opacity: 1;'> 
+				<div class='slide-text'>
+					<h4>[${q}]</h4>
+					<div class='container'>
+						<div class='row'>
+							<div class='col-sm-4'>Input</div>
+							<div class='col-sm-3'>Output</div>
+						    	<div class='col-sm-3'>Result</div>
+						</div>
+      						<div class='row'>
+							<div class='col-sm-4'>${input1}</div>
+							<div class='col-sm-3'>${output1}</div>
+							<div class='col-sm-3'>${result1}</div>
+						</div>
+						<div class='row'>
+							<div class='col-sm-4'>${input2}</div>
+							<div class='col-sm-3'>${output2}</div>
+							<div class='col-sm-3'>${result2}</div>
+					   	</div>
+						<div class='row'>
+							<div class='col-sm-4'>${input3}</div>
+							<div class='col-sm-3'>${output3}</div>
+							<div class='col-sm-3'>${result3}</div>
+					   	</div>
+					</div>
+				</div>
+			</article>`
 	
-	var questions, data;
+	var questions, code;
+	const results = new Map();
 	
 	window.onmessage = function (e) {
 	    if (e.data && e.data.language) {
-		data = e.data; 
-		console.log(data);
+		code = e.data; 
+		console.log(code);
 	    }
 	};
 	
@@ -32,20 +57,7 @@
 		let xhr = new XMLHttpRequest();
 		xhr.withCredentials = true;
 		
-		xhr.addEventListener('readystatechange', function () {
-			if (this.readyState === this.DONE) {
-				var question;
-				let response = JSON.parse(this.responseText);
-				a[numberOfQuestions] = this;
-				console.log("Teste #"+ ++numberOfQuestions + "'" +  response.stdin + "'")
-				for (question of questions[$('#question').val()]) {
-					if(question['input'] === response.stdin) {
-						break;
-					}
-				}
-				console.log(response.stdout + " === " +question['response'] + " => " + (response.stdout  === question['response']));
-			}
-		});
+		xhr.addEventListener('readystatechange', );
 		
 		xhr.open('POST', 'https://onecompiler-apis.p.rapidapi.com/api/v1/run');
 		xhr.setRequestHeader('content-type', 'application/json');
@@ -56,20 +68,23 @@
 	}
 	
 	function getData(question){
-		data.stdin = question['input'];
-		console.log(data);
-		return JSON.stringify(data, null, 2)
-		/*
-	 	return JSON.stringify({
-			'language': $('#language').val(),
-			'stdin': question['input'],
-			'files': [
-				{
-					name: 'index.js',
-					content: $('#code').val()
+		code.stdin = question['input'];
+		return JSON.stringify(code, null, 2)
+	}
+
+	function getResponse() {
+		if (this.readyState === this.DONE) {
+			let question;
+			let response = JSON.parse(this.responseText);
+			for (question of questions[$('#question').val()]) {
+				if(question['input'] === response.stdin) {
+					break;
 				}
-			]
-		});
-	 	*/
+			}
+
+			console.log(question);
+			console.log("Teste #" + "'" +  response.stdin + "'");
+			console.log(response.stdout + " === " +question['response'] + " => " + (response.stdout  === question['response']));
+		}
 	}
 }
