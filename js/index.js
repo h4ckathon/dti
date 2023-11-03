cdf147a6e6245ea47e0191e840c74a27db99897a
h4ckathon <vorltecs@gmail.com>
2023-11-03 14:27:12 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,11 +5,11 @@
 			eventType: 'populateCode',
 			language: 'javascript',
 			     files: [
-			      {
-				"name": "index.js",
-				"content": "your code...."
-			      }
-			  ]
+				    {
+				        "name": "index.js",
+				        "content": "var readline = require('readline');\r\nvar rl = readline.createInterface({\r\n  input: process.stdin,\r\n  output: process.stdout,\r\n  terminal: false\r\n});\r\n \r\nrl.on('line', function(line){\r\n   eval(line);\r\n   solve();\r\n});\r\n\r\nfunction solve(){\r\n}"
+				    }
+				]
 		}
 	)
 	languages.set('csharp',
