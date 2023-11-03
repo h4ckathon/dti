7ca0cf10a5610db0eac4ca757edc51876a7093ef
h4ckathon <vorltecs@gmail.com>
2023-11-03 17:58:27 -0300

    Update python.js

--- js/languages/python.js
+++ js/languages/python.js
@@ -7,7 +7,7 @@ var python = {
                   "content": "import sys\nimport input\n\n# Use input.var_name to retrieve stdin variables"
                 },
                 {
-                  "name": "script1.py",
+                  "name": "input.py",
                   "content": "import sys\nexec(sys.stdin.readline())"
                 }
               ]
