9e20f9be2341c167b6284ae8ef9930cd5eb229bd
h4ckathon <vorltecs@gmail.com>
2023-11-03 14:31:10 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,7 @@
 {
 	var languages = new Map();
-	languages.set('javascript',
-		{
-			eventType: 'populateCode',
-			language: 'javascript',
-			     files: [
-				    {
-				        "name": "index.js",
-				        "content": "var readline = require('readline');\r\nvar rl = readline.createInterface({\r\n  input: process.stdin,\r\n  output: process.stdout,\r\n  terminal: false\r\n});\r\n \r\nrl.on('line', function(line){\r\n   eval(line);\r\n   solve();\r\n});\r\n\r\nfunction solve(){\r\n}"
-				    }
-				]
-		}
-	)
+	$.getScript("my_lovely_script.js", () => {languages.set('javascript', javascript)});
+	
 	languages.set('csharp',
 		{
 			eventType: 'populateCode',
