35e031aa38f7db5efe8801f3d1e10befd1f4dd54
h4ckathon <vorltecs@gmail.com>
2023-11-03 20:45:47 -0300

    Update javascript.js

--- js/languages/javascript.js
+++ js/languages/javascript.js
@@ -2,9 +2,13 @@ var javascript = {
   eventType: 'populateCode',
   language: 'javascript',
        files: [
-        {
-            "name": "index.js",
-            "content": "var readline = require('readline');\r\nvar rl = readline.createInterface({\r\n  input: process.stdin,\r\n  output: process.stdout,\r\n  terminal: false\r\n});\r\n \r\nrl.on('line', function(line){\r\n   eval(line);\r\n   solve();\r\n});\r\n\r\nfunction solve(){\r\n}"
-        }
-    ]
+                {
+                  "name": "solution.js",
+                  "content": "let inputReader = require('./inputReader');\r\ninputReader.read(solve);\r\n\r\nfunction solve(){\r\n/* Implementation goes here */\r\n}"
+                },
+                {
+                  "name": "inputReader.js",
+                  "content": "/* This file should not be changed */\r\nvar readline = require('readline');\r\n\r\nconst read = (solve) => {\r\n  var rl = readline.createInterface({\r\n    input: process.stdin,\r\n    output: process.stdout,\r\n    terminal: false\r\n  });\r\n  \r\n  rl.on('line', function(line){\r\n    eval(line);\r\n    solve();\r\n  });\r\n}\r\n\r\nmodule.exports = { read };"
+                }
+              ]
 }
