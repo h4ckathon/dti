7f575ef9b745cbafb23b83337362428877969b4c
h4ckathon <vorltecs@gmail.com>
2023-11-05 11:57:14 -0300

    Update python.js

--- js/languages/python.js
+++ js/languages/python.js
@@ -2,13 +2,13 @@ var python = {
   eventType: 'populateCode',
   language: 'python',
        files: [
-                {
-                  "name": "main.py",
-                  "content": "import sys\nimport input\n\n# Use input.var_name to retrieve stdin variables\n# Implementation goes here"
-                },
-                {
-                  "name": "input.py",
-                  "content": "#This file should not be changed\nimport sys\nexec(sys.stdin.readline())"
-                }
+                  {
+                      "name": "main.py",
+                      "content": "import sys\nimport input\n\n# STDIN variables should be accessible by input.var_name\n# For instance, the following input for the program\n#   s = \"Hello World\"\n# and the code below:\n#   print(input.s)\n# will print \"Hello World\" in the Output!\n#\n# Implementation goes here!\n"
+                  },
+                  {
+                      "name": "input.py",
+                      "content": "#This file should not be changed\nimport sys\nexec(sys.stdin.readline())"
+                  }
               ]
 }
